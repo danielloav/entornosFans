778c25d5c960722daeff13b7bc4cab19aeba6541
Daniel <pipilopez8@hotmail.com>
2016-06-07 12:58:05 +0200

    jeje

--- Entornos/src/modelo/modelo.java
+++ Entornos/src/modelo/modelo.java
@@ -2,13 +2,17 @@ package modelo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import vista.tabla;
 
 public class modelo {
 	private tabla vista;
 	private Connection conexion;
+	private int resultado;
+	private int usuariocodigo;
 	
 	public void settabla(tabla tab){
 		this.vista=tab;
@@ -29,8 +33,36 @@ public class modelo {
 
 	}
 	
+	public void Insertabla(String dato) {
+		resultado = 0;
+		try {
+			Statement stmt = conexion.createStatement();
+			resultado = stmt.executeUpdate(dato);
+		} catch (SQLException e) {
+			System.out.println("Error Insertabla");
+		}
+	}
+	public int tratarcodigo(String query) {
+		try {
+			this.usuariocodigo = 0;
+			Statement stmt = conexion.createStatement();
+			ResultSet rset = stmt.executeQuery(query);
+			while (rset.next()) {
+				usuariocodigo = usuariocodigo + 1;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("error en contar filas para código");
+		}
+		return usuariocodigo;
+	}
 	public void btnanadir(String alumno, String nota, String modulo){
-		
+		int cod_al = tratarcodigo("select * from ENTORNOS.ALUMNO")+1;
+		int cod_mod = tratarcodigo("select * from ENTORNOS.modulo")+1;
+		Insertabla("Insert into ENTORNOS.alumno (COD_ALUMNO,nombre) values ("+cod_al+" , '"+alumno+"'");
+		Insertabla("Insert into ENTORNOS.TIENE_ALUMNO (COD_AL_fk,NOTA_COD_MOD,nota) values ("+cod_al+" , '"+cod_mod+"','"+nota+"'");
+		Insertabla("Insert into ENTORNOS.modulo (COD_MOD,nombre) values ("+cod_mod+" , '"+modulo+"'");
+
 	}
 
 }
--- Entornos/src/vista/tabla.java
+++ Entornos/src/vista/tabla.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import modelo.modelo;
 
@@ -18,6 +19,7 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class tabla extends JFrame {
 
@@ -27,14 +29,15 @@ public class tabla extends JFrame {
 	private JTextField txtNota;
 	private JTextField txtModulo;
 	private modelo modelo;
-
+	private DefaultTableModel dtmtabla;
+	
 	public void setmod(modelo mod) {
 		this.modelo = mod;
 	}
 
 	public tabla() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 562, 362);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -48,7 +51,8 @@ public class tabla extends JFrame {
 				String nota = txtNota.getText();
 				String modulo = txtModulo.getText();
 				modelo.btnanadir(alumno, nota, modulo);
-
+				Object[] fila = { txtAlumno.getText(), txtNota.getText(), txtModulo.getText() };
+				dtmtabla.addRow(fila);
 				txtAlumno.setText("");
 				txtNota.setText("");
 				txtModulo.setText("");
@@ -68,44 +72,67 @@ public class tabla extends JFrame {
 
 		txtModulo = new JTextField();
 		txtModulo.setColumns(10);
+		
+		JLabel lblAlumno = new JLabel("alumno");
+		
+		JLabel lblNota = new JLabel("nota");
+		
+		JLabel lblModulo = new JLabel("modulo");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup().addGap(25)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 376, GroupLayout.PREFERRED_SIZE)
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(25)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 376, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_contentPane.createSequentialGroup()
-										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-												.addGroup(gl_contentPane.createSequentialGroup()
-														.addComponent(txtAlumno, GroupLayout.PREFERRED_SIZE,
-																GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-														.addGap(18).addComponent(txtNota, GroupLayout.PREFERRED_SIZE,
-																GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-										.addGroup(gl_contentPane.createSequentialGroup().addComponent(btnAnadir)
-												.addGap(40).addComponent(btnBorrar)))
-										.addGap(51)
-										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-												.addComponent(btnModificar).addComponent(txtModulo,
-														GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-														GroupLayout.PREFERRED_SIZE))))
-				.addContainerGap(23, Short.MAX_VALUE)));
-		gl_contentPane
-				.setVerticalGroup(
-						gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup().addGap(27)
-										.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 120,
-												GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-										.addComponent(txtAlumno, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-												GroupLayout.PREFERRED_SIZE)
-								.addComponent(txtNota, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-										GroupLayout.PREFERRED_SIZE).addComponent(txtModulo, GroupLayout.PREFERRED_SIZE,
-												GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
-				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(btnAnadir)
-						.addComponent(btnBorrar).addComponent(btnModificar)).addGap(35)));
+									.addComponent(btnAnadir)
+									.addGap(40)
+									.addComponent(btnBorrar))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(txtAlumno, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblAlumno))
+									.addGap(18)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblNota)
+										.addComponent(txtNota, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+							.addGap(51)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblModulo)
+								.addComponent(btnModificar)
+								.addComponent(txtModulo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(135, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(27)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txtAlumno, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtNota, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtModulo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(31)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblAlumno)
+						.addComponent(lblNota)
+						.addComponent(lblModulo))
+					.addPreferredGap(ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnAnadir)
+						.addComponent(btnBorrar)
+						.addComponent(btnModificar))
+					.addGap(35))
+		);
 
 		table = new JTable();
+		dtmtabla = new DefaultTableModel(new Object[][] {},
+				new String[] { "CAT_FORMATO", "FORMATO", "DIMENSIONES FORMATO" });
+		//tabla.setModel(dtmtabla);
 		scrollPane.setViewportView(table);
 		contentPane.setLayout(gl_contentPane);
 	}
